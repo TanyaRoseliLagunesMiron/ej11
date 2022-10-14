04f5db2ac51d3f86a53ffef9291897a4c4ee2152
TanyaRoseliLagunesMiron <almn.tanyalagunes@cbtis66.edu.mx>
2022-10-13 19:43:59 -0500

    version11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ej11';
+  articulos = [{codigo:1, descripcion:'papas', precio:10.55},
+               {codigo:2, descripcion:'manzanas', precio:12.10},
+               {codigo:3, descripcion:'melon', precio:52.30},
+               {codigo:4, descripcion:'cebollas', precio:17},
+               {codigo:5, descripcion:'calabaza', precio:20},
+              ];
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ElementosModule } from './elementos/elementos.module';
+import { ListadoarticulosComponent } from './listadoarticulos/listadoarticulos.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoarticulosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ElementosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
